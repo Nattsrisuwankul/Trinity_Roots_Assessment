723bfb97499c63abb6b3427b31ca6502419e5b41
Natt Srisuwankul - 6609650327 <145212741+Nattsrisuwankul@users.noreply.github.com>
2026-04-02 17:14:48 +0700

    Add poll module scaffolding

---
+++ custom-addons/poll/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Polling',
+    'version': '1.0',
+    'summary': 'Polling System',
+    'sequence': 10,
+    'description': """
+Polling System
+""",
+    'category': 'Polling/Polling',
+    'website': 'https://ajscript.com',
+    'author': 'Ajscript Media',
+    'depends': [],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/poll_views.xml',
+        'views/poll_menus.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
